8e73fc5d50cc8f04eabadfc58f2910fcad0470a7
Tayeeb <tayeeb27@hotmail.com>
2023-09-11 12:07:43 +0100

    Ready to use APIs

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
 const fruitForm = document.querySelector('#inputSection form');
+const fruitList = document.querySelector('#fruitSection ul');
+
+const addFruit = fruit => {
+    const li = document.createElement('li');
+    li.textContent = fruit;
+    
+    fruitList.appendChild(li);
+    
+    li.addEventListener("click", () => {
+        fruitList.removeChild(li); 
+    });
+}
+
+fruitForm.addEventListener("submit", e => {
+    e.preventDefault();
+    addFruit(e.target.fruitInput.value);
+    e.target.fruitInput.value = "";
+})
 console.log(fruitForm);
